a7c8ff1b57758c819f698c5b5efb85920c7b2bf3
Donny Rozendal <donnyrozendal@hotmail.com>
2021-01-24 18:19:37 +0100

    Add getAdapter function and CORRECT description

--- app/src/test/java/me/ccrama/redditslide/test/BrowseTest.java
+++ app/src/test/java/me/ccrama/redditslide/test/BrowseTest.java
@@ -37,10 +37,38 @@ public class BrowseTest {
     @Mock
     Submission submission;
 
+    private SubmissionAdapter getAdapter() {
+        SettingValues.prefs = prefs;
+        return new SubmissionAdapter(context, dataSet, listView, "homepage", displayer);
+    }
+
+    /**
+     * CORRECT: SubmissionAdapter.getItemCount()
+     *
+     * Conformance: Not applicable. The item count is a simple Integer value.
+     *
+     * Ordering: Not applicable. Posts can be added at any time to the adapter.
+     *
+     * Range: The typical values received from this function are either zero, or equals to
+     * the amount of submissions in the data set + 2.
+     *
+     * Reference: This function makes use of the amount of posts in the data set whenever this
+     * amount is higher than zero. Whenever this amount of posts changes, notifyDataSetChanged()
+     * is called to notify the Adapter that the amount of data has changed. The adapter will then
+     * call getItemCount() to find out how many rows it should display.
+     *
+     * Existence: For this test, only positive numbers can exist during runtime, because
+     * a list can only be empty or contain some values. Extremely high numbers aren't supposed to
+     * be checked either. So a low value is good enough.
+     *
+     * Cardinality: The item count can only be zero or more than zero.
+     *
+     * Time: Not applicable. This function can be called at any time.
+     */
+
     @Test
     public void getItemCountZero() {
-        SettingValues.prefs = prefs;
-        SubmissionAdapter adapter = new SubmissionAdapter(context, dataSet, listView, "homepage", displayer);
+        SubmissionAdapter adapter = getAdapter();
 
         int result = adapter.getItemCount();
 
@@ -49,14 +77,13 @@ public class BrowseTest {
 
     @Test
     public void getItemCountNotZero() {
-        SettingValues.prefs = prefs;
-        SubmissionAdapter adapter = new SubmissionAdapter(context, dataSet, listView, "homepage", displayer);
-
+        SubmissionAdapter adapter = getAdapter();
         dataSet.posts = new ArrayList<>();
         dataSet.posts.add(submission);
-        int result2 = adapter.getItemCount();
 
-        assertEquals(3, result2);
+        int result = adapter.getItemCount();
+
+        assertEquals(3, result);
     }
 
 }
